decd4ff1e56d7ee3a700c65f56bf9a532ef806d9
azra247 <146413662+azra247@users.noreply.github.com>
2025-10-22 10:39:28 +0200

    Update Mutation.py

--- operators/Mutation.py
+++ operators/Mutation.py
@@ -1,18 +1,34 @@
 from core.Individu import individu
-import random
+import numpy as np
+
 
 class Mutation:
     def __init__(self, taux_mutation: float):
         self.taux_mutation = taux_mutation
 
     def mutation(self, individu: individu):
-        adn = individu.coordonnees.coordonnees_codees
-        adn_muté = []
+        """
+        Mutate bits in individu.coordonnees.coordonnees_codees with probability taux_mutation
+        """
+        bits = individu.coordonnees.coordonnees_codees.copy()
+        for i in range(len(bits)):
+            if np.random.rand() < self.taux_mutation:
+                bits[i] = 1 - bits[i]  # flip bit
 
-        for bit in adn:
-            if random.random() < self.taux_mutation:
-                adn_muté.append('1' if bit == '0' else '0')
-            else:
-                adn_muté.append(bit)
+        individu.coordonnees.coordonnees_codees = bits
+        return individu
 
-        individu.coordonnees.coordonnees_codees = adn_muté
+
+if __name__ == "__main__":
+    import numpy as np
+    from core.Coordonnees import coordonnees
+    from core.Individu import individu
+
+    c = coordonnees(np.array([1, 2]))
+    c.coordonnees_codees = np.array([1, 0, 1, 0, 1, 0])
+    ind = individu(1, c)
+
+    mutation = Mutation(taux_mutation=0.1)
+    mutated = mutation.mutation(ind)
+    print("Original:", c.coordonnees_codees)
+    print("Mutated: ", mutated.coordonnees.coordonnees_codees)
