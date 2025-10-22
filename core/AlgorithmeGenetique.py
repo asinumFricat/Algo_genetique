2b0d03e055689d93c51b14bd1a16f7f727f3c668
asinumFricat <lafondnumero3@sfr.fr>
2025-10-22 10:19:50 +0200

    Squelette algo genetique

--- core/AlgorithmeGenetique.py
+++ core/AlgorithmeGenetique.py
@@ -1,4 +1,4 @@
-from operators import Codage, Crossover, Mutation, Selection
+from operators import Codage, Crossover, Mutation, Selection, Performance
 from core import Population
 
 
@@ -7,16 +7,36 @@ class AlgorithmeGenetique:
     Classe principale de l'algorithme génétique
     """
 
-    def __init__(
-        self,
-        population: Population,
-        codage: Codage,
-        crossover: Crossover,
-        mutation: Mutation,
-        selection: Selection,
-    ):
+    def __init__(self, population: Population, codage: Codage, crossover: Crossover, mutation: Mutation, selection: Selection, performance: Performance):
         self.population = population
         self.codage = codage
         self.crossover = crossover
         self.mutation = mutation
         self.selection = selection
+        self.performance = performance
+
+    def fonctionnement(self):
+        while True :
+            parents = self.selection.selection(population)
+            enfants = self.crossover.croise(parents)
+            enfants = self.mutation.mute(enfants)
+            self.population.retire(parents)
+            self.population.ajoute(self.performance.selectionNaturelle([enfants, parents]))
+
+
+
+         
+
+
+if __name__ == "__main__" : 
+
+    #Initialisation 
+    population = Population()
+    codage = Codage()
+    crossover = Crossover()
+    mutation = Mutation()
+    selection = Selection()
+    performance = Performance()
+
+    algo = AlgorithmeGenetique(population, codage, mutation, selection, performance)
+    algo.fonctionnement()
---
+++ operators/Performance.py
@@ -0,0 +1,15 @@
+class Performance():
+
+    def __init__(self):
+        pass
+
+    def selectionNaturelle(self, parents, enfants):
+        res = parents
+        pass
+        
+
+
+
+
+
+class Parabole(Performance) : 
